ec3b135bdee5fe9fc327c4ffc3888709563f0123
Joshua Ronaldo <joshuaronaldo088@gmail.com>
2020-01-24 19:40:40 +0700

    dark mode and textfield function

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -13,12 +13,20 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func searchTextEnd(_ sender: UITextField) {
+        searchTextField.endEditing(true)
+        searchTextField.placeholder = "Are u done?"
+    }
+    
+    
+    
 
 }
 
